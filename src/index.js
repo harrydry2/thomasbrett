07a695c34b1c89590555e33fdc921a02b6b4ff24
harrydry2 <harrydry1996@gmail.com>
2019-10-26 21:19:42 +0100

    s

--- src/index.js
+++ src/index.js
@@ -1,38 +1,35 @@
 import './sass/styles.sass';
 import { $, $$ } from './modules/bling';
 
-const on = $(".speakerOn");
-const off = $(".speakerOff");
-const video = $("video");
+const on = $('.speakerOn');
+const off = $('.speakerOff');
+const video = $('video');
 
-const bottomOne = $(".bottom__one");
-const bottomOneInner = $(".bottom__one-inner");
-const bottomTwo = $(".bottom__two");
-const bottomNone = $(".bottom__none");
+const bottomOne = $('.bottom__one');
+const bottomOneInner = $('.bottom__one-inner');
+const bottomTwo = $('.bottom__two');
+const bottomNone = $('.bottom__none');
 
-on.on("click", () => {
-  on.classList.toggle("speakerNone");
-  off.classList.toggle("speakerNone");
+on.on('click', () => {
+  on.classList.toggle('speakerNone');
+  off.classList.toggle('speakerNone');
   video.muted = false;
-
 });
 
-off.on("click", () => {
-  on.classList.toggle("speakerNone");
-  off.classList.toggle("speakerNone");
+off.on('click', () => {
+  on.classList.toggle('speakerNone');
+  off.classList.toggle('speakerNone');
   video.muted = true;
 });
 
-bottomOneInner.on("click", () => {
-  bottomOne.classList.toggle("bottom__none");
-  bottomTwo.classList.toggle("bottom__none");
-  $("body").classList.toggle("noScroll");
-
+bottomOneInner.on('click', () => {
+  bottomOne.classList.toggle('bottom__none');
+  bottomTwo.classList.toggle('bottom__none');
+  $('body').classList.toggle('noScroll');
 });
 
-bottomTwo.on("click", () => {
-  bottomOne.classList.toggle("bottom__none");
-  bottomTwo.classList.toggle("bottom__none");
-  $("body").classList.toggle("noScroll");
+bottomTwo.on('click', () => {
+  bottomOne.classList.toggle('bottom__none');
+  bottomTwo.classList.toggle('bottom__none');
+  $('body').classList.toggle('noScroll');
 });
-
