2fcbe3505e15b8cceb3a4ff5133ac4f070d516b6
harrydry2 <harrydry1996@gmail.com>
2019-10-26 20:52:16 +0100

    s

--- src/index.js
+++ src/index.js
@@ -1,2 +1,38 @@
 import './sass/styles.sass';
 import { $, $$ } from './modules/bling';
+
+const on = $(".speakerOn");
+const off = $(".speakerOff");
+const video = $("video");
+
+const bottomOne = $(".bottom__one");
+const bottomOneInner = $(".bottom__one-inner");
+const bottomTwo = $(".bottom__two");
+const bottomNone = $(".bottom__none");
+
+on.on("click", () => {
+  on.classList.toggle("speakerNone");
+  off.classList.toggle("speakerNone");
+  video.muted = false;
+
+});
+
+off.on("click", () => {
+  on.classList.toggle("speakerNone");
+  off.classList.toggle("speakerNone");
+  video.muted = true;
+});
+
+bottomOneInner.on("click", () => {
+  bottomOne.classList.toggle("bottom__none");
+  bottomTwo.classList.toggle("bottom__none");
+  $("body").classList.toggle("noScroll");
+
+});
+
+bottomTwo.on("click", () => {
+  bottomOne.classList.toggle("bottom__none");
+  bottomTwo.classList.toggle("bottom__none");
+  $("body").classList.toggle("noScroll");
+});
+
